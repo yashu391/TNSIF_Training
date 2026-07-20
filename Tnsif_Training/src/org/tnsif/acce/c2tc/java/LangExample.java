ba9a8e404fcdba466861de8dba5a097232e274d6
yashu319 <yashaswini433177@gmail.com>
2026-07-20 22:58:02 +0530

    third commit
    Merge branch 'master' of https://github.com/yashu391/TNSIF_Training.git

---
+++ Tnsif_Training/src/org/tnsif/acce/c2tc/java/LangExample.java
@@ -0,0 +1,25 @@
+package org.tnsif.acce.c2tc.java;
+
+
+		public class LangExample {
+
+			public static void main(String[] args) {
+				
+				String greeting="Hello Students";
+				
+				System.out.println("Original String :"+ greeting);
+				System.out.println("Uppercase :" +greeting.toUpperCase());
+				System.out.println("Lowercase :"+greeting.toLowerCase());
+				
+				int max=Math.max(10,30);
+				double sqrt =Math.sqrt(25.0);
+				System.out.println("Max "+max);
+				System.out.println("Sqrt "+ sqrt);
+
+			}
+
+		
+
+	}
+
+
