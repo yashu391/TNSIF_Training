d13cc3f9a0ba80dae71f8620e4f5e747dfd5b79a
yashu319 <yashaswini433177@gmail.com>
2026-07-24 01:15:19 +0530

    seventh commit

--- Tnsif_Training/src/org/tnsif/acc/c2tc/oops/EncapsulationDemo.java
+++ Tnsif_Training/src/org/tnsif/acc/c2tc/oops/EncapsulationDemo.java
@@ -1,38 +1,39 @@
 package org.tnsif.acc.c2tc.oops;
 
-public class Human {
+
+class Human{
+	//variables
 	private String name;
 	private int age;
 	
-	public String getName() {
-		return name;
-	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	public String getName() {
+		return name;
+	}
 	public void setAge(int age) {
 		this.age = age;
 	}
-
+	
 	public int getAge() {
 		return age;
-	}
+	}	 
+}
+public class EncapsulationDemo {
 
 	public static void main(String[] args) {
 		Human obj=new Human();
-		obj.setName("neha");
 		obj.setAge(26);
-		System.out.println(obj.getName());	
+		obj.setName("Neha");
 		System.out.println(obj.getAge());
-		
-		obj.setName("yashu");
-		obj.setAge(20);
 		System.out.println(obj.getName());
+		
+		obj.setAge(22);
+		obj.setName("Sindhu");
 		System.out.println(obj.getAge());
+		System.out.println(obj.getName());
 	
-
 	}
 
 }
